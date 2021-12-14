72aa6f48dcee7c749540d8ed1438434c2d88e328
Ireniushka <irenerengelruiz@gmail.com>
2021-12-14 01:06:02 +0100

    auth con passport

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace AppScout\Http\Controllers\API;
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use AppScout\Http\Controllers\Controller;
+use AppScout\User;
+use Illuminate\Support\Facades\Auth; 
+use Validator;
+
+
+class AuthController extends Controller
+{ 
+    public $successStatus = 200;
+
+
+
+    /**
+     * Método para entrar en la aplicación
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login() {
+        // Se comprueba credenciales
+        if(Auth::attempt(['username' => request('username'), 'password' => request('password')])) {
+            $user = Auth::user();
+            // Se crea token de acceso
+            $success['token'] = $user->createToken('PersonalToken')->accessToken;
+            $success['dni'] = $user->dni;
+            $success['name'] = $user->name;
+            $success['username'] = $user->username;
+            $success['passChanged'] = $user->passChanged;
+            
+            return response()->json(['success' => $success], $this->successStatus);
+        }else {
+            // Se informa si surge algun error
+            return response()->json(['error' => 'Oh, parece que se produjo un error, compruebe sus credenciales'], 401);
+        }
+    }
+
+    /**
+     * Método para salir de la aplicación
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+
+    /**
+     * Método para obtener usuario conectado
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+
+
+    /**
+     * Método para recuperar contraseña
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function resetPass(Request $request) {
+        //Para marzo con los emails
+    }
+
+
+
+    /**
+     * Método para cambiar la contraseña
+     *
+     * @param int $id
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function changePass(Request $request) {
+        //Se valida formulario
+        $validator = Validator::make($request->all(), [ 
+            'old_password' => 'required|exists:users,password,dni,Auth::user()->dni',
+            'new_password' => 'required',
+            'repeat_password' => 'required|same:new_password'      
+        ]);//otra idea seria comprobar desde ionic la antigua pass y luego pasar a vista de cambio de passwd
+
+        //Se devuelve error si falla la validacion
+        if($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }else{
+            
+            $input = $request->all();
+            $input['new_password'] = bcrypt($input['new_password']);
+            
+            User::where('dni',Auth::user()->dni)
+            ->update(['password'=>$input['new_password']]);
+
+            return response()->json(['success' => $success], $this->successStatus);
+        }
+
+    }
+
+}
